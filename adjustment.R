d551a7f8d96abb244f7edea8f4b2d1f32b04a9ab
Kari Silvennoinen <kari@kalifi.org>
2025-12-09 13:12:34 +0200

    add adjustments and output in format expected by original kuitti.php

---
+++ adjustment.R
@@ -0,0 +1,35 @@
+library(readr)
+
+budget <- read_csv("budjetti-2014-he.csv",
+  col_names = c("l1", "l2", "l3", "sum")
+) |>
+  filter(!is.na(l3))
+
+# avaa legacy budjetti-2014-he-puu.csv ja mitkä rivit eroaa l3:ssa, vrt. tulojennetotus.txt
+adjusted <- read_delim(
+  "budjetti-2014-he-puu.csv",
+  delim = ";",
+  escape_double = FALSE,
+  col_names = c("l1", "l2", "l3", "adjusted_sum"),
+  trim_ws = TRUE
+) |>
+  filter(!is.na(l3), adjusted_sum > 0) |>
+  mutate(l3 = str_squish(l3))
+
+# erotus, pitäisi olla 3719802
+budget |>
+  full_join(adjusted) |>
+  filter(adjusted_sum != sum | is.na(adjusted_sum)) |>
+  mutate(diff = sum - replace_na(adjusted_sum, 0)) |>
+  summarise(diff = sum(diff))
+
+# korjaukset
+budget |>
+  full_join(adjusted) |>
+  filter(adjusted_sum != sum | is.na(adjusted_sum)) |>
+  mutate(sum = replace_na(adjusted_sum, 0)) |>
+  select(!adjusted_sum) |>
+  write_csv("budjetti-2014-he-korjaukset.csv",
+    col_names = FALSE,
+    na = ""
+  )
--- budjetti2csv.R
+++ budjetti2csv.R
@@ -5,6 +5,7 @@ library(stringr)
 library(dplyr)
 library(purrr)
 library(readr)
+library(tidyr)
 
 # Tämä on modernisoitu versio alkuperäisestä Ruby-koodista jonka toiminnasta
 # herran vuonna ei ole allekirjoittaneella juurikaan takeita,
@@ -80,9 +81,14 @@ for (i in seq_len(nrow(depts))) {
   budget <- tibble(
     l1 = dept,
     l2 = tbl |>
-      map(\(x) x[1] |> html_element("span") |> html_text()) |>
+      map(\(x) x[1] |>
+        html_element("span") |>
+        html_text()) |>
       unlist(),
-    l3 = tbl |> map(\(x) x[1] |> html_text()) |> unlist(),
+    l3 = tbl |> map(\(x) x[1] |>
+      html_text() |>
+      str_replace_all("\u00A0", " ") |>
+      str_squish()) |> unlist(),
     sum = tbl |> map(\(x) x[2] |> html_text()) |> unlist()
   ) |>
     filter(sum != "—") |>
@@ -90,18 +96,52 @@ for (i in seq_len(nrow(depts))) {
       l3 = if_else(is.na(l2), l3, NA),
       l2 = if_else(l2 != "Yhteensä", l2, NA),
       sum = str_replace_all(sum, "\\W", "") |> as.numeric()
-    )
+    ) |>
+    fill(l2)
 
   output <- bind_rows(output, budget)
 }
 
-write_csv(output, output_file, col_names = FALSE)
+write_csv(output, output_file, col_names = FALSE, na = "")
 
-# määrärahat yhteensä
-output |> filter(is.na(l2) & is.na(l3)) |> summarise(sum = sum(sum))
+# määrärahat yhteensä, 53 920 409
+output |>
+  filter(!is.na(l3)) |>
+  summarise(sum = sum(sum))
 
 # kaikkia määrärahoja ei kateta veroista vaan ne tulevat esim. Veikkaukselta,
 # EU:lta tai vastaavaa.
 # näistä on yleensä budjetissa mainittu, mutta siitä huolimatta nämä korjaukset
 # ovat erittäin epätieteelliset. Tämä osa on puhdasta käsityötä ja numeromagiaa.
 # kts. budjetti.csv korjatuille riveille
+adjustments <- read_csv("budjetti-2014-he-korjaukset.csv",
+  col_names = c("l1", "l2", "l3", "new_sum")
+)
+
+adjusted <- output |>
+  filter(!is.na(l3)) |>
+  left_join(adjustments) |>
+  mutate(sum = if_else(!is.na(new_sum), new_sum, sum)) |>
+  filter(sum > 0) |>
+  select(!new_sum)
+
+# pitäisi olla 50 200 607, eli -3719802
+adjusted |> summarise(sum = sum(sum))
+
+subtotals <- adjusted |>
+  as.data.table() |>
+  rollup(.(sum = sum(sum)), by = c("l1", "l2")) |>
+  filter(!is.na(l1))
+
+# lisää välisummat jokaisen ryhmän alkuun
+adjusted |>
+  bind_rows(subtotals) |>
+  mutate(l1_no = match(l1, unique(l1))) |>
+  group_by(l1) |>
+  mutate(l2_no = if_else(is.na(l2), 0, match(l2, unique(l2)))) |>
+  group_by(l1, l2) |>
+  mutate(l3_no = if_else(is.na(l3), 0, row_number())) |>
+  ungroup() |>
+  arrange(l1_no, l2_no, l3_no, .by_group = TRUE) |>
+  select(!c(l1_no, l2_no, l3_no)) |>
+  write_csv("budjetti.csv", col_names = FALSE, na = "")
